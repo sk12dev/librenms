7ac260f048d03ea4b28c09dc31f0cec515100a0f
sk12dev <andy@solutionk12.com>
2026-01-09 10:37:19 -0500

    5

--- resources/views/react-dashboard/index.blade.php
+++ resources/views/react-dashboard/index.blade.php
@@ -9,31 +9,31 @@
 @endsection
 
 @section('javascript')
-    {{-- Set config in head to ensure it's available before module loads --}}
+    {{-- Set config in head AND window to ensure it's available --}}
     <script>
-        (function() {
-            window.ReactDashboardConfig = {
-                apiToken: {!! json_encode($apiToken) !!},
-                apiUrl: {!! json_encode($apiUrl) !!},
-                baseUrl: {!! json_encode($baseUrl) !!},
-                csrfToken: {!! json_encode(csrf_token()) !!},
-            };
-            console.log('[Blade Template] ReactDashboardConfig set in head:', {
-                apiUrl: window.ReactDashboardConfig.apiUrl,
-                apiToken: window.ReactDashboardConfig.apiToken ? window.ReactDashboardConfig.apiToken.substring(0, 8) + '...' : 'NULL/UNDEFINED',
-                baseUrl: window.ReactDashboardConfig.baseUrl,
-                hasApiToken: !!window.ReactDashboardConfig.apiToken,
-                hasApiUrl: !!window.ReactDashboardConfig.apiUrl,
-                rawApiToken: window.ReactDashboardConfig.apiToken,
-            });
-        })();
+        window.ReactDashboardConfig = {
+            apiToken: {!! json_encode($apiToken) !!},
+            apiUrl: {!! json_encode($apiUrl) !!},
+            baseUrl: {!! json_encode($baseUrl) !!},
+            csrfToken: {!! json_encode(csrf_token()) !!},
+        };
+        console.log('[Blade Template] ReactDashboardConfig set:', {
+            apiUrl: window.ReactDashboardConfig.apiUrl,
+            apiToken: window.ReactDashboardConfig.apiToken ? window.ReactDashboardConfig.apiToken.substring(0, 8) + '...' : 'NULL',
+            baseUrl: window.ReactDashboardConfig.baseUrl,
+        });
     </script>
 @endsection
 
 @section('content')
 <div class="container-fluid" style="padding: 0;">
-    {{-- React Dashboard Container --}}
-    <div id="react-dashboard-root"></div>
+    {{-- React Dashboard Container with config in data attributes as backup --}}
+    <div 
+        id="react-dashboard-root"
+        data-api-token="{{ $apiToken }}"
+        data-api-url="{{ $apiUrl }}"
+        data-base-url="{{ $baseUrl }}"
+    ></div>
 </div>
 @endsection
 
